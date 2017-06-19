d256bd2870c655293aebccad0c79d648f025c514
SvenBenoot <sven.benoot@student.howest.be>
2017-06-19 13:10:43 +0200

    programma aangepast

--- Eindwerk_pyCode.py
+++ Eindwerk_pyCode.py
@@ -1,3 +1,10 @@
+##########################################################
+# Made by Sven Benoot Last modified 18/06/2017 17:11     #
+#                                                        #
+# To login on the server pleace time the pi ip adress +  #
+# :5000 in the webbrowser.                               #
+##########################################################
+
 from flask import Flask, render_template, request, redirect
 from Klassen.I2CLCDklasse import i2cLCD
 from Klassen.OneWireSensorKlasse import OneWireSensor
@@ -6,13 +13,29 @@ from Klassen.class_db import DbClass
 from Klassen.MCPklasse import SPI
 import RPi.GPIO as GPIO
 import threading
+import socket
 import time
 
+# global variables
 State = 0
 StateWeergave = "Automatic"
 
+# setting up classes
+# database class
 db = DbClass()
+# LCD class
+LCD = i2cLCD(0x27, 16)
+LCD.lcd_init()
+# ADC class
+MCP = SPI()
+# servo class
+servo = Servo(18, 50)
+servo.init()
+# temperature sensor class
+onewire1 = OneWireSensor('/sys/bus/w1/devices/28-000008c5ac92/w1_slave')
+onewire2 = OneWireSensor('/sys/bus/w1/devices/28-000008e097d8/w1_slave')
 
+# configuring I/O
 GPIO.setmode(GPIO.BCM)
 fan = 16
 pump = 12
@@ -25,20 +48,25 @@ GPIO.setup(Rled, GPIO.OUT)
 GPIO.setup(Gled, GPIO.OUT)
 GPIO.setup(Bled, GPIO.OUT)
 
-LCD = i2cLCD(0x27, 16)
-LCD.lcd_init()
+# show ip+port on LCD screen
+try:
+    ip_address = ''
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip_address = s.getsockname()[0]
+    s.close()
+    LCD.lcd_string("IP + PORT :5000", 0)
+    LCD.lcd_string(ip_address, 1)
+    print(ip_address)
+except:
+    LCD.lcd_string("IP + PORT :5000", 0)
+    LCD.lcd_string("169.254.10.11", 1)
 
-MCP = SPI()
-
-servo = Servo(18, 50)
-servo.init()
-
-onewire1 = OneWireSensor('/sys/bus/w1/devices/28-000008c5ac92/w1_slave')
-onewire2 = OneWireSensor('/sys/bus/w1/devices/28-000008e097d8/w1_slave')
-
+# starting of flask server
 app = Flask(__name__)
 
 
+# method for closing the flask server
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
@@ -46,6 +74,10 @@ def shutdown_server():
     func()
 
 
+# method that runs in the baground when program start
+# this method is active when the operation mode is set to automatic
+# the method reads the sensor values and decides when it is to hot ore the humidity is to low
+# and then activates irrigation system, fan, LED, or roof hatch
 def MainProgram():
     t = threading.currentThread()
     print("###Hoofdprogramma gestart###")
@@ -84,6 +116,7 @@ def MainProgram():
     print("###Hoofdprogramma gestopt###")
 
 
+# this method saves data from the sensors in to the database
 def DataLogging():
     t = threading.currentThread()
     print("###Datalogging Gestart###")
@@ -99,12 +132,14 @@ def DataLogging():
     print("###Datalogging gestopt###")
 
 
+# this starsts the two background programs
 t = threading.Thread(target=MainProgram)
 t.start()
 t2 = threading.Thread(target=DataLogging)
 t2.start()
 
 
+# this is the homepage
 @app.route('/')
 def Home():
     global StateWeergave
@@ -115,6 +150,7 @@ def Home():
     return render_template("Home.html", Temp=temp, Humidity=humidity, StateTemp=statetemp, StateHumidity=statehumidity, state=StateWeergave)
 
 
+# this is for getting the button values of the homepage
 @app.route('/setGPIO', methods=['POST'])
 def handle_data():
     global State, StateWeergave, t
@@ -123,6 +159,9 @@ def handle_data():
         State = int(tekst)
         if State == 0:
             StateWeergave = "Automatic"
+            GPIO.output(Rled, GPIO.LOW)
+            GPIO.output(Bled, GPIO.LOW)
+            GPIO.output(Gled, GPIO.LOW)
             t = threading.Thread(target=MainProgram)
             t.start()
 
@@ -130,6 +169,9 @@ def handle_data():
             StateWeergave = "Manual"
             t.do_run = False
             t.join()
+            GPIO.output(Rled, GPIO.LOW)
+            GPIO.output(Bled, GPIO.LOW)
+            GPIO.output(Gled, GPIO.LOW)
 
     if State == 1:
         if tekst == "11":
